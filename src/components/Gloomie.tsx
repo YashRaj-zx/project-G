2fa3a84fcc1f2021a605d9e655c752186b9d953f
vijayramaraju10072010 <vijayramaraju10072010@gmail.com>
2025-08-10 10:34:05 +0000

    Updated

--- src/components/Gloomie.tsx
+++ src/components/Gloomie.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gloomie: React.FC = () => {
   return (
-    <div className="fixed bottom-4 right-4 z-50 hidden">
+    <div className="fixed bottom-4 right-4 z-50">
       <img src="/ghost-icon.png" alt="Gloomie the Ghost" className="h-16 w-16" />
     </div>
   );
